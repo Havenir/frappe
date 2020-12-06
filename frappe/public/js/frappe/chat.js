0e060ca85bac10f156fd2ddcfb7e58547fae4cde
Kartik Sharma <kartiksharma9319@gmail.com>
2020-12-06 12:03:11 +0000

    hotfix: Chat notification and Seperate window for ERP chat box

--- frappe/public/js/frappe/chat.js
+++ frappe/public/js/frappe/chat.js
@@ -1591,15 +1591,15 @@ class extends Component {
 				// Let's check whether notification permissions have already been granted
 				else if (Notification.permission === "granted") {
 					// If it's okay let's create a notification
-					const notification = new Notification(`${user}: ${r.content}`, options)
+					const notification = new Notification(`${frappe.user.first_name(r.user)}: ${r.content}`, options)
 				}
 				// Otherwise, we need to ask the user for permission
 				else if (Notification.permission !== "denied") {
 					Notification.requestPermission().then(function (permission) {
-					// If the user accepts, let's create a notification
-					if (permission === "granted") {
-						const notification = new Notification(`${user}: ${r.content}`, options)
-					}
+						// If the user accepts, let's create a notification
+						if (permission === "granted") {
+							const notification = new Notification(`${frappe.user.first_name(r.user)}: ${r.content}`, options)
+						}
 					});
 				}
 			}
